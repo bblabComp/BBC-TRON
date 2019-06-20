7e8501244161d4da1672317945fe82007c513cd7
nitesh8130 <nitesh.kumar@bblab.biz>
2019-06-20 13:56:53 +0530

    Merge branch 'staging'

--- src/service/TransactionService.js
+++ src/service/TransactionService.js
@@ -16,23 +16,23 @@ exports.processBlock = async (res, processBlockNum) => {
     for(let key in res.transactions){
         var trxns = res.transactions[key].raw_data.contract[0];
         if(trxns.type === 'TransferContract'){
-            console.log(':::: TransferContract :::')
-            var toAdd = tronweb.address.fromHex(trxns.parameter.value.to_address);
+            console.log('TransferContract :::')
+            var toAddress = await tronweb.address.fromHex(trxns.parameter.value.to_address);
+            
             //Check the address is present in database or not
-            await WalletRepository.findAddress(toAdd).then(result => {
-                if(result.data!=null){
-                    var transactionBody = {
-                        toAddress: toAdd,
-                        amount: trxns.parameter.value.amount,
-                        owner_address: tronweb.address.fromHex(trxns.parameter.value.owner_address),
-                        processBlockNum : processBlockNum,
-                        txID : res.transactions[key].txID
-                    }
-                    checkForBinanceDeposit(transactionBody, result);
+            const userWalletInfo = await WalletRepository.findAddress(toAddress);
+            if(userWalletInfo.data!=null){
+                var transactionBody = {
+                    toAddress: toAddress,
+                    amount: trxns.parameter.value.amount,
+                    owner_address: tronweb.address.fromHex(trxns.parameter.value.owner_address),
+                    processBlockNum : processBlockNum,
+                    txID : res.transactions[key].txID
                 }
-            }).catch(err => {
-                console.log('Something goes Wrong to find adddres in database ::: ', err);
-            })
+                checkForBinanceDeposit(transactionBody, userWalletInfo);
+            }else{
+                console.error('Address Not Found in our Local database ::: ', userWalletInfo.err)
+            }
         }
     }
 }
@@ -43,21 +43,23 @@ exports.processBlock = async (res, processBlockNum) => {
  *              and enable --> send amount to Binane Organization wallet.
  */
 async function checkForBinanceDeposit(transactionBody, addressInfo){
-    return await axios.get(config.MAIN_URL+'/currency/binance/deposit').then((result) => {
-        if(!result.data.isSuccess){
+    await axios.get(config.MAIN_URL+'/currency/binance/deposit').then(result => {
+        console.log("check for binance deposit ::: ", result);
+        if(result.status == 200 && !result.data.isSuccess){
             tronweb.trx.sendTransaction(config.ORG_ADDRESS, transactionBody.amount, addressInfo.data.privateKey).then(result => {
                 if(result){
                     incomingTransaction(transactionBody);
                 }
             }).catch(err => {
-                console.log('something goes worng during transfer to organization wallet ::: ', err);
+                console.log('Error While Transfering Amount to Organization Wallet ::: ', err);
             });
         }else{
             incomingTransaction(transactionBody);
         }
-    }).catch((error) => {
-        console.log('something worng during call api ::::', error);
+    }).catch(err => {
+        saveUserIncomingTrxInMongoDb(transactionBody);
     });
+    
 }
 
 /**
@@ -68,32 +70,18 @@ async function checkForBinanceDeposit(transactionBody, addressInfo){
 * 
 */
 async function incomingTransaction(transactionBody){
-    console.log('incomming transaction ::: ', config.MAIN_URL+'/deposit/tron');
-    return await axios.post(config.MAIN_URL+'/deposit/tron', {
+    console.log('Process Incomming Transaction ::: ', config.MAIN_URL+'/deposit/tron');
+    await axios.post(config.MAIN_URL+'/deposit/tron', {
         toAddress : transactionBody.toAddress,
         fromAddress : transactionBody.owner_address,
         tranId: transactionBody.txID,
         amount: transactionBody.amount
-    }).then((res) => {
-        if(res){
-            console.log('deposit tron amount to the user wallet ::::');
+    }).then(result => {
+        if(result != null){
+            console.log("Amount Deposit to the user wallet ::: Success");
         }
-        return res;
-    }).catch((err) => {
-        console.log('getting error during axios call', err)
-        var tranInfo = {
-            fromAddress : transactionBody.owner_address,
-            toAddress : transactionBody.toAddress,
-            amount : transactionBody.amount,
-            blockNum : transactionBody.processBlockNum,
-            tranId : transactionBody.txID,
-            status : 'PENDING',
-            createdAt: new Date(),
-            lastModified: new Date()
-        }
-        const item = TransactionRepository.postDeposit(tranInfo);
-        doTemplating.loadTemplate();
-        return item;
+    }).catch(err => {
+        saveUserIncomingTrxInMongoDb(transactionBody);
     });
 }
 
@@ -102,11 +90,36 @@ async function incomingTransaction(transactionBody){
  */
 exports.saveNowBlock = async (nowBlockNum, prevBlockNum) => {
     //save now block in db ;
-    await syncBlock.updateBlockNum(nowBlockNum, prevBlockNum).then(result => {
-        console.log('-----------------------');
+    return await syncBlock.updateBlockNum(nowBlockNum, prevBlockNum).then(result => {
+        return result;
     }).catch(error => {
         console.log('Error while saving current block ::: ')
     });
+}
+
+function saveUserIncomingTrxInMongoDb(transactionBody){
+    TransactionRepository.findByTxnId(transactionBody.txID).then(result => {
+        if(result.data == null){
+            console.log("Error While Deposit Amount in user wallet ::: ", result.data);
+            var tranInfo = {
+                fromAddress : transactionBody.owner_address,
+                toAddress : transactionBody.toAddress,
+                amount : transactionBody.amount,
+                blockNum : transactionBody.processBlockNum,
+                tranId : transactionBody.txID,
+                status : 'PENDING',
+                createdAt: new Date(),
+                lastModified: new Date()
+            }
+            const item = TransactionRepository.postDeposit(tranInfo);
+            doTemplating.loadTemplate();
+            return item;
+        }else{
+            console.log('Transation id already exit ::: ');
+        }
+    }).catch(err => {
+        console.log('Error while finding exiting Transaction ::: ')
+    })
 }
 
 /**
